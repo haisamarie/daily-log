2397a7a696aacf6efb3e14eaded1da43d40ae460
haisamarie <marie.199204@gmail.com>
2025-09-14 10:23:00 +0900

    feat: add props to Hero component for dynamic title and description

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,18 +1,30 @@
 import Image from 'next/image';
 import styles from './styles.module.css';
 
-const Hero = () => {
+type Props = {
+  title?: string;
+  description?: string;
+  imageUrl?: string;
+};
+
+const Hero = ({
+  title,
+  description = ' キャッチアップした技術的な内容をアップしていきます。',
+  imageUrl = '/images/hero.png',
+}: Props) => {
   return (
     <div className={styles.hero}>
       <div className={styles.heroText}>
-        <h1 className={styles.heroTitle}>
-          <span className={styles.strong}>鞠</span>の記録
-        </h1>
-        <p className={styles.heroDescription}>
-          キャッチアップした技術的な内容をアップしていきます。
-        </p>
+        {title ? (
+          <h1 className={styles.heroTitle}>{title}</h1>
+        ) : (
+          <h1 className={styles.heroTitle}>
+            <span className={styles.strong}>鞠</span>の記録
+          </h1>
+        )}
+        <p className={styles.heroDescription}>{description}</p>
       </div>
-      <Image className={styles.heroImage} src="/images/hero.png" alt="" width={1400} height={420} />
+      <Image className={styles.heroImage} src={imageUrl} alt="" width={1400} height={420} />
     </div>
   );
 };
