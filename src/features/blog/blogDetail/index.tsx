430e05d172bc78ca173efbfd14156b6b31dbba7a
haisamarie <marie.199204@gmail.com>
2025-09-19 21:23:35 +0900

    fix

--- src/features/blog/blogDetail/index.tsx
+++ src/features/blog/blogDetail/index.tsx
@@ -1,14 +1,11 @@
 import Hero from '@/components/Hero';
 import Main from '@/components/Main';
-import Button from '@/components/Button';
 
 const BlogDetail = () => {
   return (
     <>
       <Hero title="blog title" imageUrl="/images/pc.jpg" />
-      <Main>
-        <Button type="button">もっと見る</Button>
-      </Main>
+      <Main>aaaa</Main>
     </>
   );
 };
