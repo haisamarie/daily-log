a7eb42b8bf9fb0e751c35e09db21636fa8574415
haisamarie <marie.199204@gmail.com>
2025-09-20 14:36:59 +0900

    fix

--- src/utils/data/post.ts
+++ src/utils/data/post.ts
@@ -11,8 +11,6 @@ type Post = {
 // 記事の場所を指定
 const postDirectory = path.join(process.cwd(), 'content/posts');
 
-//投稿数
-
 //記事データを取得する関数
 export async function getPostData() {
   const fileNames = fs.readdirSync(postDirectory);
@@ -22,6 +20,7 @@ export async function getPostData() {
       const fullPath = path.join(postDirectory, fileName);
       const fileContents = fs.readFileSync(fullPath, 'utf-8');
       const matterResult = matter(fileContents);
+      console.log(matterResult);
       return {
         slug,
         ...(matterResult.data as Omit<Post, 'slug'>),
