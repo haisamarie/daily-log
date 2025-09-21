3cf6aadf1c0eb359c6e3bb5259bd7651393920a8
haisamarie <marie.199204@gmail.com>
2025-09-21 21:00:50 +0900

    fix

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,22 +7,14 @@ type Item = {
   slug: string;
   title: string;
   date: string;
-  description?: string;
   thumbnail?: string;
 };
 
 const Card = ({ items }: CardProps) => {
   return (
     <div className={styles.card}>
-      {items.map(({ slug, title, date, description, thumbnail }) => (
-        <CardItem
-          slug={slug}
-          key={slug}
-          title={title}
-          date={date}
-          description={description}
-          thumbnail={thumbnail}
-        />
+      {items.map(({ slug, title, date, thumbnail }) => (
+        <CardItem slug={slug} key={slug} title={title} date={date} thumbnail={thumbnail} />
       ))}
     </div>
   );
